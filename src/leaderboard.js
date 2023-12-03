0b64c9a904df5d63f07eeba1e4abbe5668a5f05b
Antony Fantapié <fantapie.antony@gmail.com>
2023-12-03 23:18:46 +0100

    misc: embellish code

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -12,9 +12,8 @@ const countMessages = (messages) => {
 
 const display = (messageCountArray, leaderboardLength) => {
     console.log(`Top ${leaderboardLength} senders (data from February 2023) :`);
-    for (let i = 0; i < leaderboardLength && i < messageCountArray.length; i++) {
-        console.log(`${i+1} - ${messageCountArray[i][0]} : ${messageCountArray[i][1]} messages`);
-    }
+    for (let i = 0; i < leaderboardLength && i < messageCountArray.length; i++)
+        console.log(`${i + 1} - ${messageCountArray[i][0]} : ${messageCountArray[i][1]} messages`);
 }
 
 export const leaderboard = (dataPath, leaderboardLength) => {
