92b8772091025b8bcd8ee59cef68c09883540789
Antony Fantapié <fantapie.antony@gmail.com>
2024-11-02 22:31:55 +0100

    misc: change base data to July 2023

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -11,7 +11,7 @@ const countMessages = (messages) => {
 }
 
 const display = (messageCountArray, leaderboardLength) => {
-    console.log(`Top ${leaderboardLength} senders (data from February 2023) :`);
+    console.log(`Top ${leaderboardLength} senders (data from July 2023) :`);
     for (let i = 0; i < leaderboardLength && i < messageCountArray.length; i++)
         console.log(`${i + 1} - ${messageCountArray[i][0]} : ${messageCountArray[i][1]} messages`);
 }
