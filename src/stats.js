37b089101ca40fe85ad08a26590718a1834a82a5
Antony Fantapié <fantapie.antony@gmail.com>
2024-11-02 22:32:23 +0100

    feat: add top 3 reactions receiver / giver to the chat stats summary

--- src/stats.js
+++ src/stats.js
@@ -8,6 +8,30 @@ let mostReactedMessage = {
 }
 let messageCount = 0;
 let messageCountPerMember = [];
+let topReactionsGiver = [];
+let topReactionsGiverArray = [];
+let topReactionsReceiver = [];
+let topReactionsReceiverArray = [];
+
+const countReactionsPerMember = (message) => {
+    if (message.reactions) {
+        message.reactions.forEach(reaction => {
+            topReactionsGiver[reaction.actor] = (topReactionsGiver[reaction.actor] || 0) + 1;
+        })
+    }
+    topReactionsGiverArray = Object.entries(topReactionsGiver);
+    topReactionsGiverArray.sort((a, b) => b[1] - a[1]);
+}
+
+const countReactionsReceivedPerMember = (message) => {
+    if (message.reactions) {
+        message.reactions.forEach(reaction => {
+            topReactionsReceiver[message.sender_name] = (topReactionsReceiver[message.sender_name] || 0) + 1;
+        })
+    }
+    topReactionsReceiverArray = Object.entries(topReactionsReceiver);
+    topReactionsReceiverArray.sort((a, b) => b[1] - a[1]);
+}
 
 const checkTop3Members = (messageCountPerMember) => {
     const messageCountPerMemberArray = Object.entries(messageCountPerMember);
@@ -34,6 +58,8 @@ const gatherData = (messages, argv) => {
             if (message.timestamp_ms >= argv[3] && message.timestamp_ms <= argv[4]) {
                 countMessagesPerMember(message);
                 checkMostReactedMessage(message);
+                countReactionsPerMember(message);
+                countReactionsReceivedPerMember(message);
                 messageCount++;
             }
         })
@@ -41,6 +67,8 @@ const gatherData = (messages, argv) => {
         messages.forEach(message => {
             countMessagesPerMember(message);
             checkMostReactedMessage(message);
+            countReactionsPerMember(message);
+            countReactionsReceivedPerMember(message);
             messageCount++;
         })
     }
@@ -50,6 +78,14 @@ const display = () => {
     console.log('Top 3 most active members :')
     for (let i = 0; i < top3Members.length; i++)
         console.log(`${i + 1} - ${top3Members[i][0]} : ${top3Members[i][1]} messages`);
+    console.log('\n')
+    console.log('Top 3 reactions giver :')
+    for (let i = 0; i < 3 && i < topReactionsGiverArray.length; i++)
+        console.log(`${i + 1} - ${topReactionsGiverArray[i][0]} : ${topReactionsGiverArray[i][1]} reactions`);
+    console.log('\n')
+    console.log('Top 3 reactions receiver :')
+    for (let i = 0; i < 3 && i < topReactionsReceiverArray.length; i++)
+        console.log(`${i + 1} - ${topReactionsReceiverArray[i][0]} : ${topReactionsReceiverArray[i][1]} reactions`);
     console.log('\n')
     console.log(`Most reacted message (from ${mostReactedMessage.sender_name} with ${mostReactedMessage.reactions.length} reactions): \n\n${mostReactedMessage.content}`);
     console.log('\n')
